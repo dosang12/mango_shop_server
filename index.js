8db8367fd1a464fe15f60dcce99e9fed554fc8bb
dosang12 <knight2671@gmail.com>
2023-03-21 15:34:02 +0900

    서버 경로 수정

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const multer = require("multer");
 const { response } = require("express");
 
 const app = express();
-const port = 8080;
+const port = "https://port-0-mango-shop-server2-p8xrq2mlfg43w7v.sel3.cloudtype.app/";
 
 const upload = multer({
   storage: multer.diskStorage({
